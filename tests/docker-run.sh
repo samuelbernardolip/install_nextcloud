a485dbdaa8c06238da9eb25e904ff410fd4ebc5d
Samuel Bernardo <samuel@lip.pt>
2018-12-10 16:28:27 +0000

    correct error when running ansible playbook
    Signed-off-by: Samuel Bernardo <samuel@lip.pt>

--- tests/docker-run.sh
+++ tests/docker-run.sh
@@ -2,6 +2,8 @@
 # exit if any command fails
 set -e
 
+printf '[defaults]\nroles_path=%s/' "$1" >ansible.cfg
+
 ansible-playbook $1/test.yml -i inventory --syntax-check
 ansible-playbook $1/test.yml -i inventory
 curl -i -s --insecure --data "user=ncadmin&password=" https://localhost/index.php/login | egrep -q "Location:.*/apps/files/"
