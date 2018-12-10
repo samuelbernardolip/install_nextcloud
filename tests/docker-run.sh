d4d1884b93ea55c3f0128f7e2432f378f5c92774
Samuel Bernardo <samuel@lip.pt>
2018-12-10 16:08:30 +0000

    correct relative path to test.yml in docker exec bash script
    Signed-off-by: Samuel Bernardo <samuel@lip.pt>

--- tests/docker-run.sh
+++ tests/docker-run.sh
@@ -2,6 +2,6 @@
 # exit if any command fails
 set -e
 
-ansible-playbook test.yml -i inventory --syntax-check
-ansible-playbook test.yml -i inventory
+ansible-playbook ./test.yml -i inventory --syntax-check
+ansible-playbook ./test.yml -i inventory
 curl -i -s --insecure --data "user=ncadmin&password=" https://localhost/index.php/login | egrep -q "Location:.*/apps/files/"
